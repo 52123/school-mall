ae10968ae47dba53b7a39330bf56a277ac042a6a
vitamin <644396066@qq.com>
2019-06-26 17:56:54 +0800

    perf(ObjectKeyCache): reduce network transmission

--- school-mall-common/src/main/java/com/hugh/common/annotation/impl/ObjectKeyCacheAspect.java
+++ school-mall-common/src/main/java/com/hugh/common/annotation/impl/ObjectKeyCacheAspect.java
@@ -33,12 +33,12 @@ public class ObjectKeyCacheAspect {
 
     private final RedisService redisService;
 
-    public ObjectKeyCacheAspect(@Autowired RedisService redisService){
+    public ObjectKeyCacheAspect(@Autowired RedisService redisService) {
         this.redisService = redisService;
     }
 
     @Around("@annotation(com.hugh.common.annotation.ObjectKeyCache)")
-    private Object around(ProceedingJoinPoint joinPoint) throws Throwable{
+    private Object around(ProceedingJoinPoint joinPoint) throws Throwable {
 
         // 获取注解所在类名
         Class<?> className = joinPoint.getTarget().getClass();
@@ -47,41 +47,47 @@ public class ObjectKeyCacheAspect {
         String methodName = joinPoint.getSignature().getName();
 
         // 获取方法签名的类类型和参数类型
-        Class<?>[] paramClass = ((MethodSignature)joinPoint.getSignature()).getParameterTypes();
+        Class<?>[] paramClass = ((MethodSignature) joinPoint.getSignature()).getParameterTypes();
         Object[] params = joinPoint.getArgs();
 
         String redisKey;
         try {
             // 得到访问方法的对象
             Method method = className.getMethod(methodName, paramClass);
-            if(method.isAnnotationPresent(ObjectKeyCache.class)){
+            if (method.isAnnotationPresent(ObjectKeyCache.class)) {
                 ObjectKeyCache objectKeyCache = method.getAnnotation(ObjectKeyCache.class);
                 // 若readObject为false，返回key，true，则拼接属性值作为key
                 redisKey = objectKeyCache.readObject() ? getRedisKey(params, objectKeyCache) : getAnnotationKey(objectKeyCache);
-                if(redisService.hasKey(redisKey)){
-                    return redisService.getValue(redisKey);
-                }else{
+
+               /*
+                * 一次查询返回，减少网络传输次数
+                */
+                Object obj = redisService.existKey(redisKey);
+                if (obj != null) {
+                    return obj;
+                } else {
                     Object resp = joinPoint.proceed();
                     redisService.setValue(redisKey, resp);
                     return resp;
                 }
             }
-        }catch (Exception e){
-            log.error("ObjectKeyCache error",e);
+        } catch (Exception e) {
+            log.error("ObjectKeyCache error", e);
         }
         return null;
     }
 
     /**
-     *  若方法无参数，直接返回注解中的key
+     * 若方法无参数，直接返回注解中的key
+     *
      * @param objectKeyCache 注解
      * @return redisKey
      */
-    private String getAnnotationKey(ObjectKeyCache objectKeyCache) throws Exception{
+    private String getAnnotationKey(ObjectKeyCache objectKeyCache) throws Exception {
         String key = objectKeyCache.key();
         String[] fields = objectKeyCache.fields();
 
-        if(fields.length != 0){
+        if (fields.length != 0) {
             throw new Exception("Empty method signature cannot have fields");
         }
         return key;
@@ -89,12 +95,13 @@ public class ObjectKeyCacheAspect {
 
     /**
      * 根据ObjectKeyCache生成key
-     * @param params 方法签名
+     *
+     * @param params         方法签名
      * @param objectKeyCache 注解
      * @return redisKey
      */
-    private String getRedisKey(Object[] params, ObjectKeyCache objectKeyCache) throws Exception{
-        if(params.length == 0){
+    private String getRedisKey(Object[] params, ObjectKeyCache objectKeyCache) throws Exception {
+        if (params.length == 0) {
             throw new Exception("Method signature is not empty or you can set the readObject value to false");
         }
 
@@ -103,19 +110,19 @@ public class ObjectKeyCacheAspect {
         String[] fields = objectKeyCache.fields();
 
         // 注解没有值时，根据对象生成redisKey
-        if(StringUtils.isBlank(key) && fields.length == 0){
+        if (StringUtils.isBlank(key) && fields.length == 0) {
             return generateDefaultKey(param);
         }
 
         // 只有fields时，根据提供的fields生成redisKey
-        if(StringUtils.isBlank(key) && fields.length != 0){
+        if (StringUtils.isBlank(key) && fields.length != 0) {
             return generateFieldsKey(param, fields);
         }
 
         // 有key也有fields时
-        if(StringUtils.isNotBlank(key) && fields.length != 0){
+        if (StringUtils.isNotBlank(key) && fields.length != 0) {
             // 若数量不匹配，抛出异常
-            if(fields.length != StringUtils.countMatches(key,REGEX)){
+            if (fields.length != StringUtils.countMatches(key, REGEX)) {
                 throw new Exception("{} does not match the number of fields");
             }
             return generateKey(param, key, fields);
@@ -125,14 +132,15 @@ public class ObjectKeyCacheAspect {
     }
 
     /**
-     *  生成自定义的redisKey
-     * @param param 方法第一个签名
-     * @param key 输入的key
+     * 生成自定义的redisKey
+     *
+     * @param param  方法第一个签名
+     * @param key    输入的key
      * @param fields 期望对象属性
      * @return redisKey
      */
     @SuppressWarnings("unchecked")
-    private String generateKey(Object param, String key, String[] fields) throws Exception{
+    private String generateKey(Object param, String key, String[] fields) throws Exception {
         Class clazz = param.getClass();
         StringBuilder redisKey = new StringBuilder(key);
         boolean hasInvalidValue = false;
@@ -145,7 +153,7 @@ public class ObjectKeyCacheAspect {
             Method method = clazz.getMethod("get" + firstCharToUpperCase(field));
             String value = String.valueOf(method.invoke(param));
             //若得到的值为null，则去父类那里看看是否有对应的属性值，若还是null，则跳过
-            if("null".equals(value) || StringUtils.isBlank(value)){
+            if ("null".equals(value) || StringUtils.isBlank(value)) {
                 hasInvalidValue = true;
                 continue;
             }
@@ -157,27 +165,28 @@ public class ObjectKeyCacheAspect {
 
 
     /**
-     *  根据非空字段值生成redisKey
-     *  如果方法没有参数、对象为null、字段全为空，则抛出异常
+     * 根据非空字段值生成redisKey
+     * 如果方法没有参数、对象为null、字段全为空，则抛出异常
+     *
      * @return redisKey
      */
     @SuppressWarnings("unchecked")
-    private String generateDefaultKey(Object param) throws Exception{
+    private String generateDefaultKey(Object param) throws Exception {
 
         Class clazz = param.getClass();
         StringBuilder redisKey = new StringBuilder();
-        for(Field field : clazz.getFields()){
+        for (Field field : clazz.getFields()) {
             // 获取到对象对应字段的get方法并从中获取值
             Method method = clazz.getMethod("get" + firstCharToUpperCase(field.getName()));
             // 不等于null时，添加到redisKey中
             String value = String.valueOf(method.invoke(param));
-            if(!"null".equals(value) && StringUtils.isNotBlank(value)){
+            if (!"null".equals(value) && StringUtils.isNotBlank(value)) {
                 redisKey.append(value).append(":");
             }
         }
 
         int length = redisKey.length();
-        if(length == 0){
+        if (length == 0) {
             throw new Exception("the first object must has at least one valid value");
         }
 
@@ -186,22 +195,23 @@ public class ObjectKeyCacheAspect {
     }
 
     /**
-     *  根据对象属性生成相应的redisKey
-     * @param param 方法签名对象
+     * 根据对象属性生成相应的redisKey
+     *
+     * @param param  方法签名对象
      * @param fields 字段
      * @return redisKey
      * @throws Exception 无对应属性时抛出异常
      */
     @SuppressWarnings("unchecked")
-    private String generateFieldsKey(Object param, String[] fields) throws Exception{
+    private String generateFieldsKey(Object param, String[] fields) throws Exception {
         Class clazz = param.getClass();
         StringBuilder redisKey = new StringBuilder();
-        for(String field : fields){
+        for (String field : fields) {
             // 获取到对象对应字段的get方法并从中获取值
             Method method = clazz.getMethod("get" + firstCharToUpperCase(field));
             // 不等于null时，添加到redisKey中
             String value = String.valueOf(method.invoke(param));
-            if(!"null".equals(value) && StringUtils.isNotBlank(value)){
+            if (!"null".equals(value) && StringUtils.isNotBlank(value)) {
                 redisKey.append(value).append(":");
             }
         }
@@ -212,39 +222,41 @@ public class ObjectKeyCacheAspect {
 
 
     /**
-     *  删去多余的{}
+     * 删去多余的{}
+     *
      * @param redisKey 带有多余{}的键
      * @return redisKey
      */
     private String deleteRegex(String redisKey) {
         // 除去多余的 :{}
-        while(redisKey.contains(REGEX_WITH_COLON)){
+        while (redisKey.contains(REGEX_WITH_COLON)) {
             redisKey = redisKey.replace(REGEX_WITH_COLON, "");
         }
 
-        while(redisKey.contains(REGEX)){
-            redisKey =  redisKey.replace(REGEX,"");
+        while (redisKey.contains(REGEX)) {
+            redisKey = redisKey.replace(REGEX, "");
         }
 
         return redisKey;
     }
 
     /**
-     *  将传入字段的首字母转为大写
+     * 将传入字段的首字母转为大写
+     *
      * @param name 字段名
      * @return 首字母大写的字段名
      * @throws Exception 若首字符不为合法字母则抛出异常
      */
-    private String firstCharToUpperCase(String name) throws Exception{
+    private String firstCharToUpperCase(String name) throws Exception {
         char firstChar = name.charAt(0);
 
         //首字母为大写直接返回
-        if(A <= firstChar &&  firstChar <= Z){
+        if (A <= firstChar && firstChar <= Z) {
             return name;
         }
 
-        if(LOWERCASE_A <= firstChar && firstChar <= LOWERCASE_Z){
-            firstChar = (char)(firstChar - 32);
+        if (LOWERCASE_A <= firstChar && firstChar <= LOWERCASE_Z) {
+            firstChar = (char) (firstChar - 32);
             return firstChar + name.substring(1);
         }
 
