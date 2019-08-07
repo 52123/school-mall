92e17c01ff71c77b69578974820468ac2d4eddbc
vitamin <644396066@qq.com>
2019-08-07 15:27:35 +0800

    perfect(RpcProxy): reduce useless network transmission

--- school-mall-rpc/src/main/java/com/hugh/rpc/client/RpcProxy.java
+++ school-mall-rpc/src/main/java/com/hugh/rpc/client/RpcProxy.java
@@ -3,10 +3,8 @@ package com.hugh.rpc.client;
 import com.hugh.rpc.protocol.RpcRequest;
 import com.hugh.rpc.protocol.RpcResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 
 import java.lang.reflect.Proxy;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -20,8 +18,6 @@ public class RpcProxy {
     private ServiceDiscover discover;
 
     private static final AtomicInteger ATOMIC_INT = new AtomicInteger(1);
-
-    private static final ConcurrentHashMap<String, RpcClient> RPC_CLIENT_MAP = new ConcurrentHashMap<>();
 
     public RpcProxy(ServiceDiscover discover) {
         this.discover = discover;
@@ -40,22 +36,9 @@ public class RpcProxy {
                     request.setParameterTypes(method.getParameterTypes());
 
                     /*
-                     * 获取服务的地址(随机)
-                     * 当地址不同的时候才重新初始化Netty客户端
-                     */
-                    String serviceAddress = discover.getServiceAddress();
-                    RpcClient rpcClient = RPC_CLIENT_MAP.get(serviceAddress);
-                    if (rpcClient == null) {
-                        String address = serviceAddress.split(":")[0];
-                        int port = Integer.valueOf(serviceAddress.split(":")[1]);
-                        rpcClient = new RpcClient().init(address, port);
-                        RPC_CLIENT_MAP.put(serviceAddress, rpcClient);
-                    }
-
-                    /*
                      * 创建基于Netty实现的RpcClient连接服务端并发送请求
                      */
-                    RpcResponse response = rpcClient.send(request).get();
+                    RpcResponse response = discover.getRpcClient().send(request).get();
                     if (response.isError()) {
                         log.error("PRC接收返回消息失败:", response.getMsg());
                         throw new Exception(response.getMsg());
--- school-mall-rpc/src/main/java/com/hugh/rpc/client/ServiceDiscover.java
+++ school-mall-rpc/src/main/java/com/hugh/rpc/client/ServiceDiscover.java
@@ -3,7 +3,9 @@ package com.hugh.rpc.client;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.curator.framework.CuratorFramework;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -20,26 +22,45 @@ public class ServiceDiscover {
 
     private String serviceName;
 
+    private final List<String> list = new ArrayList<>();
+
     private final AtomicInteger atomicInteger = new AtomicInteger(1);
 
-    public ServiceDiscover(CuratorFramework client, String rootPath, String serviceName) {
+    private static final ConcurrentHashMap<String, RpcClient> RPC_CLIENT_MAP = new ConcurrentHashMap<>();
+
+    public ServiceDiscover(CuratorFramework client, String rootPath, String serviceName) throws Exception{
         this.client = client;
         this.rootPath = rootPath;
         this.serviceName = serviceName;
+        getServiceAddress();
     }
 
-    public String getServiceAddress() throws Exception {
-        List<String> list = client.getChildren().forPath(rootPath + "/" + serviceName);
-        int listSize = list.size();
-        if (listSize == 0) {
-            throw new Exception("未能找到指定服务");
-        }
+    private void getServiceAddress() throws Exception {
+        list.addAll(client.getChildren().forPath(rootPath + "/" + serviceName));
+    }
 
+    public RpcClient getRpcClient() throws Exception{
         /*
          * 若服务有多个地址，实现负载均衡
          * 假如listSize等于1时，减一之后进行异或永远为另一个数(atomicInteger)
          * 所以要加个判断处理
          */
-        return list.get(listSize == 1 ? 0 : atomicInteger.getAndAdd(1) ^ (listSize - 1));
+        int listSize = list.size();
+        if (listSize == 0) {
+            throw new Exception("未能找到指定服务");
+        }
+        String serviceAddress = list.get(listSize == 1 ? 0 : atomicInteger.getAndAdd(1) ^ (listSize - 1));
+        return RPC_CLIENT_MAP.computeIfAbsent(serviceAddress,ServiceDiscover::createClient);
+    }
+
+    private static RpcClient createClient(String serviceAddress){
+        String address = serviceAddress.split(":")[0];
+        int port = Integer.valueOf(serviceAddress.split(":")[1]);
+        try {
+             return  new RpcClient().init(address, port);
+        } catch (Exception e) {
+            log.error("RPC-Client初始化失败",e);
+            return null;
+        }
     }
 }
